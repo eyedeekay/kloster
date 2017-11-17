ac28302fd18564cdd9d9489923d29a119b29a9b4
idk <problemsolver@openmailbox.org>
2017-11-16 20:11:38 -0500

    seeing if docker@v3.6 will install in the iso

--- config.mk
+++ config.mk
@@ -3,6 +3,8 @@ export mirror = http://dl-cdn.alpinelinux.org/alpine/
 #export branch = edge
 export branch = v3.6
 
+#export extra_repository = "--extra-repository http://dl-cdn.alpinelinux.org/alpine/edge/main"
+
 define edge_extra_repository
 --extra-repository http://dl-cdn.alpinelinux.org/alpine/v3.6/main
 --extra-repository http://dl-cdn.alpinelinux.org/alpine/v3.6/community
