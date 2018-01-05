42d5d4f99af4d6c0b5f4da7782a17803a9f57420
idk <problemsolver@openmailbox.org>
2018-01-04 20:48:13 -0500

    crap I broke it

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 
 export mirror = http://dl-cdn.alpinelinux.org/alpine/
-#export branch = edge
-export branch = v3.6
+export branch = edge
+#export branch = v3.6
 
 #export extra_repository = "--extra-repository http://dl-cdn.alpinelinux.org/alpine/edge/main"
 
