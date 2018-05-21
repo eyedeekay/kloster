bd765aaac25319c0c9550afa7eb64e7f1a1d7665
idk <problemsolver@openmailbox.org>
2018-05-21 15:22:17 -0400

    make install

--- pvs.mk
+++ pvs.mk
@@ -89,7 +89,7 @@ docker-iso:
 
 docker-xl-create: directory
 	mount $(HDD_PATH)/alpine-docker-$(branch)-x86_64.iso $(HDD_PATH)/disk/boot/
-	xl create /etc/xen/docker.install.cfg -c 'export MKFS_OPTS_BOOT="-O ^64bit"; setup-alpine'
+	xl create /etc/xen/docker.install.cfg -c 'export MKFS_OPTS_BOOT="-O ^64bit"; setup-alpine -q'
 	umount $(HDD_PATH)/alpine-docker-$(branch)-x86_64.iso $(HDD_PATH)/disk/boot/
 
 docker-registry-iso:
